fc40fe6601c4bc18a35c54714a0f0cd0f3d1fb70
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2017-11-16 09:45:32 +0100

    as presented at group meeting

--- MultiAmp.R
+++ MultiAmp.R
@@ -1,10 +1,12 @@
 library(MultiAmplicon)
 library(dada2)
 library(parallel)
+library(reshape)
+library(pheatmap)
 
 FILTER <- FALSE
 newMA <- FALSE
-
+newTax <- FALSE
 
 a.files <- list.files(path="/SAN/Ines_longitudinal/NGS3/results/Unaligned_Mismatch0/fastq files/",
                       pattern=".fastq.gz", full.names=TRUE)
@@ -69,3 +71,59 @@ if (newMA){
     save(STnoC, file="/SAN/Ines_longitudinal/Seqtable.Rdata")
 } else {load(file="/SAN/Ines_longitudinal/Seqtable.Rdata")} ## -> STnoC
 
+
+## sort for the primers
+makes.prot <- ifelse(grepl("18S|proti", names(STnoC)), FALSE, TRUE)
+                     
+prot.dada.seq <- DNAStringSet(unlist(lapply(STnoC[makes.prot], colnames)))
+nuc.dada.seq <- DNAStringSet(unlist(lapply(STnoC[!makes.prot], colnames)))
+
+
+### identify failed samples
+sumSample <- lapply(STnoC, rowSums)
+dadaMapped <- melt(sumSample)
+dadaMapped$sample <- unlist(lapply(sumSample, names))
+
+Dmap <- t(cast(dadaMapped, sample~L1))
+Dmap[is.na(Dmap)] <- 0
+
+pdf("figures/primers_dadaMap.pdf",
+    width=75, height=15, onefile=FALSE)
+pheatmap(log10(Dmap+1))
+dev.off()
+
+plot.exclude.clusters <- function(OTUs, k=2) {
+    distOTUs <- hclust(dist(t(log10(OTUs+1))))
+    clusCatOTU <- factor(unlist(cutree(distOTUs, k = k)))
+    ## plot
+    plot(distOTUs)
+    ## add cluster highlighting
+    rect.hclust(distOTUs, k=k)
+    return(clusCatOTU)
+}
+
+pdf("figures/dada_sample_exclusion_test.pdf", width=24, height=12)
+cluster.table.sample <- plot.exclude.clusters(Dmap, 4)
+dev.off()
+
+## probably one hast to include 1 and 2, maybe even 4...
+
+
+### Taxonomy
+##### TAXONOMY assignment
+assign.full.tax <- function(seqtab){
+    seqs <- getSequences(seqtab)
+    taxa <- assignTaxonomy(seqs, "/SAN/db/RDP/Silva_123/SILVA_123_dada2_WHexp.fasta")
+    return(taxa)
+    ## taxaS <- addSpecies(taxa, "/SAN/db/RDP/Silva_123/SILVA_123_dada2_WHexp.fasta", verbose=TRUE)
+    ## return(taxaS)
+}
+
+set.seed(100) # Initialize random number generator for reproducibility
+if(newTax){
+    tax.l <- mclapply(STnoC[!makes.prot], assign.full.tax, mc.cores=20)
+    save(tax.l, file="/SAN/Ines_longitudinal/tax.Rdata")
+} else{load(file="/SAN/Ines_longitudinal/tax.Rdata")} ## -> tax.l
+
+
+writeFasta(prot.dada.seq, "/SAN/Ines_longitudinal/dada_prot.fasta")
